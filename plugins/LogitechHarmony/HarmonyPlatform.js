5466a5571c8674a4899d81d343aeb438b0faa5cb
Thomas Kluge <th.kluge@me.com>
2017-02-14 21:31:25 +0100

    some cosmetics

--- plugins/LogitechHarmony/HarmonyPlatform.js
+++ plugins/LogitechHarmony/HarmonyPlatform.js
@@ -294,7 +294,7 @@ HarmonyPlatform.prototype.buildCCUObjectList = function(dispatched_request,editI
 																	  				  "lamp_index":lightdevice["index"],
 																					  "hm_device_type":type,
 																	  				  "ctype":lightdevice["ctype"],
-																					  "adress":lightdevice["adress"]});
+																					  "adress":lightdevice["adress"] ||Ê"Browse"});
 			}
 	});
 	return fakeLights;
