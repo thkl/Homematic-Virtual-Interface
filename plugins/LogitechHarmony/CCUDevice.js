1af55df6e2de296ca7f2c13f99ac633e63eb4960
Thomas Kluge <th.kluge@me.com>
2017-02-15 19:32:02 +0100

    no message

---
+++ plugins/LogitechHarmony/CCUDevice.js
@@ -0,0 +1,89 @@
+'use strict'
+
+//
+//  FakeHueDevice.js
+//  Homematic Virtual Interface Plugin
+//
+//  Created by Thomas Kluge on 10.12.16.
+//  Copyright © 2016 kSquare.de. All rights reserved.
+//
+
+var Fakelight = require(__dirname + '/device/Fakelight.js').Fakelight;
+var HomematicDevice;
+
+var hmtypes = {"0":{"type":"HM-LC-Sw1-Pl","channel":"SWITCH"},
+			   "1":{"type":"HM-LC-Dim1T-Pl","channel":"DIMMER"}};
+
+
+var CCUDevice = function (hueserver,fobject) {
+	this.hueserver = hueserver;
+	this.log = hueserver.log;
+	this.light;
+	this.hmDevice;
+	this.bridge = hueserver.bridge;
+	this.name = fobject["name"]
+	this.objType = fobject["type"]
+	this.index = fobject["index"]
+	this.adress = fobject["adress"]
+	this.ctype = fobject["ctype"]
+	this.isReal = false;
+	this.rpcClient = this.hueserver.plugin.hm_layer.addRPCClient("BidCos-RF")
+	this.log.debug("Fake Data %s",fobject)
+	this.init();
+}	
+	
+	
+CCUDevice.prototype.init = function() {
+  var that = this;
+  this.light = new Fakelight(this.log,this.index,this.name,false)
+  this.initRealDevice()
+  this.hueserver.addLightDevice(this);
+}
+
+CCUDevice.prototype.initRealDevice = function(hmtype) {
+  var that = this
+  this.light.on("harmony_device_value_change", function(lightid,parameter,state){
+	  that.log.debug("Event -> Set %s to %s Object Type ",parameter,state,that.objType);
+
+	  if (that.objType == "3") {
+	  // HM Device
+	  if ((parameter=="bri") && (state > 0)) {
+		if (that.ctype == "DIMMER") {
+			that.bridge.callRPCMethod("BidCos-RF","setValue",[that.adress,"LEVEL",{"explicitDouble":(state/255)}], function(error, value) {});
+  		}
+	  }	   
+	  
+	  if (parameter == "on") {
+		
+		if (that.ctype == "SWITCH") {
+			that.bridge.callRPCMethod("BidCos-RF","setValue",[that.adress,"STATE",state], function(error, value) {});
+		}
+		
+		if (that.ctype == "DIMMER") {
+			if (state == false) {
+				that.bridge.callRPCMethod("BidCos-RF","setValue",[that.adress,"LEVEL",{"explicitDouble":0}], function(error, value) {});
+			} // Do Not switch to 1 with the ON Message
+  		}
+
+	  }
+	  
+	  }
+	  
+	  // HM Program
+ 	 if (that.objType == "4") {
+	 	if ((parameter=="on") && (state==true)) {
+	 		that.bridge.runRegaScript("var x = dom.GetObject('" +  that.adress.slice(2) + "');if (x) {x.ProgramExecute();}");
+	 	}
+	 }
+	 
+	 
+  })
+}
+
+CCUDevice.prototype.getType = function() {
+  return this.hmDevice.type;
+}
+
+module.exports = {
+  FakeHueDevice : CCUDevice
+}
