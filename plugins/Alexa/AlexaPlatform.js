cc2c55882a93afc8a498a3b811022041564b0a47
Thomas Kluge <th.kluge@me.com>
2017-02-19 13:43:17 +0100

    no message

--- plugins/Alexa/AlexaPlatform.js
+++ plugins/Alexa/AlexaPlatform.js
@@ -592,14 +592,16 @@ AlexaPlatform.prototype.nextLogicName = function(callback) {
     Object.keys(this.alexa_appliances).forEach(function (key) {
 	  var applicance = that.alexa_appliances[key];
 	  if ((applicance.isVirtual) && (applicance.isVirtual==true)) {
-        var num = applicance.name.match(/[1-9][0-9]*/)
-		that.log.debug("%s",JSON.stringify(num))
-        if (num.length>0) {
-			var n = parseInt(num[0])
-			if (n>lastnum) {
-				lastnum = n
+        if (applicance.name) {
+        	var num = applicance.name.match(/[1-9][0-9]*/)
+			that.log.debug("%s",JSON.stringify(num))
+			if (num.length>0) {
+				var n = parseInt(num[0])
+				if (n>lastnum) {
+					lastnum = n
+				}
 			}
-        }
+      	}
       }
     })
 	lastnum = lastnum + 1
